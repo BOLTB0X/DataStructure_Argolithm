e7d1bd42ca4ea09b137b6699c84500f31b1a37ec
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-29 14:05:26 +0900

    백트래킹

---
+++ BackTraking/중복순열.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int arr[5] = { 1,2,3,4,5 };
+bool visited[5];
+int per[5];
+
+void print_arr(void) {
+	for (int i = 0; i < 2; i++) {
+		cout << per[i] << ' ';
+	}
+	return;
+}
+void DFS(int depth) {
+	if (depth == 2) {
+		print_arr();
+		cout << '\n';
+		return;
+	}
+	for (int i = 0; i < 5; i++) {
+		visited[i] = true;
+		per[depth] = arr[i];
+		DFS(depth + 1);
+		visited[i] = false;
+	}
+}
+
+int main(void) {
+	DFS(0);
+	return 0;
+}
---
+++ BackTraking/중복조합.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int arr[5] = { 1,2,3,4,5 };
+bool visited[5];
+int com[5];
+
+void print_com(void) {
+	for (int i = 0; i < 3; i++) {
+		cout << com[i] << ' ';
+	}
+	return;
+}
+
+void DFS(int idx,int depth) {
+	if (3 == depth) {
+		print_com();
+		cout << '\n';
+		return;
+	}
+	for (int i = idx; i < 5; i++) {
+		visited[i] = true;
+		com[depth] = arr[i];
+		DFS(i,depth + 1);
+		visited[i] = false;
+	}
+}
+
+int main(void) {
+	//DFS·Î ¼ø¿­
+	DFS(0,0);
+}
