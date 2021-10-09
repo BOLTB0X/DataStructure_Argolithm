7746ff35091d8673f655e8ed612f95c8f874468f
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-09 12:39:06 +0900

    소수판별

--- Math/소수 판별.cpp
+++ Math/소수 판별.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 bool is_prime_number(int n) {
-    if (n==0 || n==1)
-        return false;
+	if (n==0 || n==1)
+        	return false;
 	for (int i = 2; i <(int)(sqrt(n)+1); i++) {
 		if (n % i == 0)
 			return false;
