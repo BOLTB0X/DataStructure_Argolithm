5614eb1a999e6d4eac95ea1bce6b4f1a6feca8dc
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-10 15:09:10 +0900

    다음순열

---
+++ Math/다음순열.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+vector<int> per;
+
+//다음순열
+bool next_permutation(vector<int>& per, int n) {
+	int i = n - 1;
+	while (i > 0 && per[i - 1] >= per[i]) i -= 1;
+	if (i <= 0) return false;
+	int j = n - 1;
+	while (per[j] <= per[i - 1]) j -= 1;
+	int temp = per[i - 1];
+	per[i - 1] = per[j];
+	per[j] = temp;
+	j = n - 1;
+	while (i < j) {
+		temp = per[i];
+		per[i] = per[j];
+		per[j] = temp;
+		i += 1;
+		j -= 1;
+	}
+	return true;
+}
