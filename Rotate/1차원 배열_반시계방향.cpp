afe9f486c919ced3594301427199c6d143625823
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-24 14:17:50 +0900

    회전

--- Rotate/1차원 배열_반시계방향.cpp
+++ Rotate/1차원 배열_반시계방향.cpp
@@ -15,9 +15,11 @@ void print_arr(void) {
 
 int main(void) {
 	print_arr();
+	
 	//라이브러이 이용
 	//반시계방향으로 n만큼 이동
 	rotate(arr, arr + 1, arr + 10);
+	
 	print_arr();
 	//2 3 4 5 6 7 8 9 10 1
 	return 0;
