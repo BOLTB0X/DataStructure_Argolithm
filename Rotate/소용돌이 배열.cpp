72c354367f6ac04a9cba5924faa2a384e6d4b833
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-12-02 16:14:29 +0900

    소용돌이

---
+++ Rotate/소용돌이 배열.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[1000][1000] = { 0, }; 
+
+void rotate(int n) {
+    int cnt = 1; // 초기값을 1로.. 
+
+    int column_l = 0;  
+    int column_r = n - 1; 
+    int row_u = 0; 
+    int row_d = n - 1; 
+    int x = 0, y = 0; 
+
+    while (cnt <= n * n) {
+        for (x = column_l; x <= column_r && cnt <= n * n; x++) 
+            arr[y][x] = cnt++;
+        x--;
+        row_u++;
+
+        for (y = row_u; y <= row_d && cnt <= n * n; y++) 
+            arr[y][x] = cnt++;
+        y--;
+        column_r--;
+
+        for (x = column_r; x >= column_l && cnt <= n * n; x--) 
+            arr[y][x] = cnt++;
+        x++;
+        row_d--;
+
+        for (y = row_d; y >= row_u && cnt <= n * n; y--) 
+            arr[y][x] = cnt++;
+        y++;
+        column_l++;
+    }
+
+    return;
+}
+
+void solution(void) {
+    int n;
+
+    cin >> n;
+
+    rotate(n);
+    
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+    return;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	solution();
+	return 0;
+}
