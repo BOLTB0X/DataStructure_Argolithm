fbf8ad673643ad067fd3912cb829a09267c76f63
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-01-23 22:48:56 +0900

    substr

--- string/substr.cpp
+++ string/substr.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int main(void) {
 	string st = "s1tring";
+	//substr startidx는 0으로 초기화 되어있음 --> 시작,cnt로 기억
 	cout << st.substr(0, 2) << '\n';
-	cout << st.substr(2,0) << '\n';
+	cout << st.substr(2,0) << '\n'; //2부터 0까지
 	cout << st.substr(1) << '\n';
 	return 0;
 }
