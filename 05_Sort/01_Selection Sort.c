10ef7672ecb2b5e1fdaf096318befd18e2732488
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-24 14:02:31 +0900

    선택정렬

--- 05_Sort/01_Selection Sort.c
+++ 05_Sort/01_Selection Sort.c
@@ -1,35 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include<limits.h>
-#define SIZE 1000
+#define Size 1000
 
-int arr[SIZE];
-int swap(int *a,int *b){
-	int temp = *a;
+int arr[Size];
+
+void swap(int* a, int* b) {
+	int tmp = *a;
 	*a = *b;
-	*b = temp;
+	*b = tmp;
+
+	return;
+}
+
+void select_Sort(int n) {
+	int min_idx;
+	for (int i = 0; i < n - 1; ++i) {
+		min_idx = i;
+		for (int j = i; j < n; ++j) {
+			if (arr[min_idx] > arr[j]) 
+				min_idx = j;
+		}
+		swap(&arr[i], &arr[min_idx]);
+	}
+
+	return;
 }
 
 int main(void) {
-	int n, min, index;
+	int n;
+
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++) 
+	for (int i = 0; i < n; ++i)
 		scanf("%d", &arr[i]);
-	for (int i = 0; i < n; i++) {
-		//최댓값으로 셋팅
-		min = INT_MAX; 
-		for (int j = i; j < n; j++) {
-			if (min > arr[j]) {
-				min = arr[j];
-				index = j;
-			}
-		}
-		swap(&arr[i], &arr[index]);
-	}
-	for (int i = 0; i < n; i++) {
+
+	select_Sort(n);
+
+	for (int i = 0; i < n; ++i)
 		printf("%d ", arr[i]);
-	}
-	printf("\n");
-	system("pause");
+
 	return 0;
 }
