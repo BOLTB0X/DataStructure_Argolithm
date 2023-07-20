8796999b05f2dcf844239f1bd0533e54aab83536
B0X <83914919+BOLTB0X@users.noreply.github.com>
2023-07-20 17:18:38 +0900

    commit

--- 프로그래머스/KaKao/성격 유형 검사하기.swift
+++ 프로그래머스/KaKao/성격 유형 검사하기.swift
@@ -1,33 +1,35 @@
 // 2022 KAKAO TECH INTERNSHIP 성격 유형 검사하기
-import Foundation
+iimport Foundation
 
 func solution(_ survey:[String], _ choices:[Int]) -> String {
     var answer:String = ""
-    var choicesSocre: [Int] = [0,3,2,1,0,1,2,3]
-    var jipyo = ["R": 0, "T": 0, "C": 0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0]
+    let choicesScore:[Int] = [0, 3, 2, 1, 0, 1, 2, 3]
+    var typeDic:[String:Int] = ["R": 0, "T": 0, "C":0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0]
     
-    for (i, sur) in survey.enumerated() {
-        let tmp = sur.map{String($0)}
-        
-        if choices[i] > 4 {
-            jipyo[tmp[1]]! += choicesSocre[choices[i]]
-        } else if choices[i] < 4 {
-            jipyo[tmp[0]]! += choicesSocre[choices[i]]
-        } else {
+    for (i, s) in survey.enumerated() {
+        let splitedS = s.map{ String($0) }
+        if choices[i] == 4 {
             continue
+        } else if choices[i] > 4 { // 동의
+            typeDic[splitedS[1]]! += choicesScore[choices[i]]
+        } else {
+            typeDic[splitedS[0]]! += choicesScore[choices[i]]
+        }
+        //print(i, splitedS)
+    }
+    
+    //print(typeDic)
+    
+    for jipyo in ["RT", "CF", "JM", "AN"] {
+        let a = String(jipyo.first!)
+        let b = String(jipyo.last!)
+        
+        if typeDic[a]! < typeDic[b]! {
+            answer += b
+        } else {
+            answer += a
         }
     }
     
-    //print(jipyo)
-    for key in ["RT", "CF", "JM", "AN"] {
-        let front = String(key.first!)
-        let back = String(key.last!)
-        
-        if jipyo[front]! < jipyo[back]! {
-            answer += back
-        } else {
-            answer += front
-        }
-    }
     return answer
 }
