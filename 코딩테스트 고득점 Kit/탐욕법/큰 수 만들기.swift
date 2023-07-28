5480ed6d38a1d654b4cc4ebc8a5ce9943cce1915
B0X <83914919+BOLTB0X@users.noreply.github.com>
2023-07-28 10:33:41 +0900

    commit

--- 코딩테스트 고득점 Kit/탐욕법/큰 수 만들기.swift
+++ 코딩테스트 고득점 Kit/탐욕법/큰 수 만들기.swift
@@ -1,28 +1,24 @@
 import Foundation
 
 func solution(_ number:String, _ k:Int) -> String {
-    // 스택을 이용
-    var stack:[Int] = []
-    var number = number.map{ Int(String($0))!} // 편의용
-    var cnt:Int = 0 // 지운 횟수
+    var answer:String = ""
+    var number = number.map { String($0) }
+    var k = k
+    var stack:[String] = []
     
-    for i in 0..<number.count {
-        // 스택에 지울 후보인 숫자들을 넣어 현재 들어가는 수보다 작으면 pop
-        while stack.count > 0 && stack.last! < number[i] && cnt < k {
-            stack.removeLast() // 스택 상단
-            cnt += 1 // 카운트
-        }
-        stack.append(number[i])
-    }
-    
-    // 만약 하나도 안지워지는 경우
-    // 맨 뒤부터 k번 지움
-    if cnt == 0 {
-        for _ in 1...k {
+    for n in number {
+        while !stack.isEmpty && stack.last! < n && k > 0 {
             stack.removeLast()
+            k -= 1
         }
+        stack.append(n)
     }
     
-    // 남아있는 스택 원소들을 전부 문자열로 붙여줌
-    return stack.reduce("") { $0 + "\($1)"}
+    // 다 안지워질수도 있으므로
+    if k > 0 {
+        stack = Array(stack[0..<stack.count-k])
+    }
+    
+    answer = stack.joined()
+    return answer
 }
