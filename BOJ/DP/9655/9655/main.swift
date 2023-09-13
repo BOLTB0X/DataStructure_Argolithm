b8c1d3f2ffccbab12095155a23a3d26732063826
B0X <83914919+BOLTB0X@users.noreply.github.com>
2023-09-13 21:12:37 +0900

    commit

---
+++ BOJ/DP/9655/9655/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  9655 돌 게임
+//  https://www.acmicpc.net/problem/9655
+//  Created by KyungHeon Lee on 2023/09/13.
+//
+
+import Foundation
+
+let N = Int(readLine()!)!
+print(N % 2 == 0 ? "CY" : "SK")
