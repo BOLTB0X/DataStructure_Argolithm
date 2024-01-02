acdbac9c92604bfb89d235b8135a5d8045b9d0fc
B0X <83914919+BOLTB0X@users.noreply.github.com>
2024-01-02 23:14:03 +0900

    commit

--- BOJ/그래프/최단경로/1753.cpp
+++ BOJ/그래프/최단경로/1753.cpp
@@ -1,49 +1,47 @@
 // 1753 최단 경로
 // https://www.acmicpc.net/problem/1753
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
 #define INF 98765432
 
 using namespace std;
 
 int V, E, K;
-
 vector<pair<int,int>> adj[20001];
-vector<int> dist;
 
-void solution(void) {
+void dijkstra(void) {
+    int answer = 0;
+    vector<int> dist(V+1, INF);
     priority_queue<pair<int,int>> pq;
-    dist.resize(V+1, INF);
 
     pq.push({0, K});
     dist[K] = 0;
 
-    while (!pq.empty()) {
-        int curDist = -pq.top().first;
+    while (!pq.empty())
+    {
+        int curCost = -pq.top().first;
         int curNode = pq.top().second;
         pq.pop();
 
-        if (dist[curNode] < curDist) continue;
+        if (dist[curNode] < curCost) continue;
 
         for (auto next: adj[curNode]) {
-            int nextDist = next.second;
+            int nextCost = next.second;
             int nextNode = next.first;
 
-            if (dist[nextNode] <= nextDist + curDist) continue;
+            if (dist[nextNode] <= curCost + nextCost) continue;
 
-            dist[nextNode] = curDist + nextDist;
+            dist[nextNode] = curCost + nextCost;
             pq.push({-dist[nextNode], nextNode});
         }
-
     }
-
-    for (int i = 1; i <= V; ++i)
-    {
+    
+    for (int i = 1; i <= V; ++i) {
         if (dist[i] == INF) cout << "INF" << '\n';
         else cout << dist[i] << '\n';
     }
-    
+
     return;
 }
 
@@ -51,15 +49,15 @@ int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int a, b, c;
 
     cin >> V >> E >> K;
     for (int i = 0; i < E; ++i) {
-        cin >> a >> b >> c;
+        int a, b, c;
 
+        cin >> a >> b >> c;
         adj[a].push_back({b, c});
     }
 
-    solution();
+    dijkstra();
     return 0;
 }
