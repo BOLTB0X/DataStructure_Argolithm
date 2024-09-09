89f4753b994c18eaeec6cf65a752718cebd88393
B0X <83914919+BOLTB0X@users.noreply.github.com>
2024-09-09 21:25:49 +0900

    정리

--- BOJ/Greedy/1946/1946.py
+++ BOJ/Greedy/1946/1946.py
@@ -1,24 +1,27 @@
-# 1946 신입사원
+# 1946 신입 사원
 # https://www.acmicpc.net/problem/1946
 import sys
+
 input = sys.stdin.readline
 
-T = int(input())
 
-for _ in range(T):
-    N = int(input())
-    applys = [list(map(int, input().split())) for _ in range(N)]
-    answer = 0
+def solution(n, applicants):
+    ret = 1
 
-    applys.sort(key=lambda x: x[0])
+    applicants.sort(key=lambda x: x[0])
+    cmp = applicants[0][1]
 
-    minCmp = 0
+    for i in range(1, n):
+        if applicants[i][1] < cmp:
+            ret += 1
+            cmp = applicants[i][1]
 
-    for i in range(N):
-        if i == 0:
-            answer += 1
-            minCmp = applys[i][1]
-        elif applys[i][1] < minCmp:
-            answer += 1
-            minCmp = applys[i][1]
-    print(answer)
+    return ret
+
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    applicants = [list(map(int, input().split())) for _ in range(n)]
+    print(solution(n, applicants))
